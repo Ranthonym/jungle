dd5ed78db336024e38611bd834be43e38ebac965
Rahul Anthony Mitra <rahul.anthonym@gmail.com>
2020-04-10 23:20:07 +0000

    add model spec and user auth credential class method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,22 @@
 class User < ActiveRecord::Base
   
   has_secure_password
+
+  validates :email, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, length: { minimum: 6 }
+
+  before_save :normalize_email 
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.downcase.strip)
+    user && user.authenticate(password)
+  end
   
+  private
+  def normalize_email
+    self.email = email.downcase.strip
+  end
+
 end
